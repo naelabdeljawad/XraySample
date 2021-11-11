a05ae7a2a61cd0a3c815ab9a4e9293e42df53a22
Nael Marwan <nael.abdeljawad@gmail.com>
2021-11-11 20:13:52 +0200

    Add files via upload

---
+++ SampleProject/src/main/java/com/page_objects/Page.java
@@ -0,0 +1,11 @@
+package com.page_objects;
+
+public class Page {
+    public boolean getTrue() {
+        return true;
+    }
+
+    public boolean getFalse() {
+        return false;
+    }
+}
---
+++ SampleProject/src/test/java/com/tests/Definition.java
@@ -0,0 +1,28 @@
+package com.tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class Definition {
+
+    private boolean expectedValue;
+    private boolean actualValue;
+
+    @Given("Expected value is True")
+    public void expected_value_is_true() {
+        expectedValue = true;
+    }
+
+    @When("Actual value is really {string}")
+    public void actual_value_is_really(String value) {
+        actualValue = Boolean.valueOf(value);
+    }
+
+    @Then("Assert actual and expected values")
+    public void assert_actual_and_expected_values() {
+        Assert.assertEquals(actualValue, expectedValue);
+    }
+
+}
